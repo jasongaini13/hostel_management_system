6a2ee360ad7ab8ece4a15e20a9efa7d7551b6ab7
jasongaini13 <b421022@iiit-bh.ac.in>
2024-07-01 14:06:20 +0530

    Add .gitattributes file and resolve line endings

--- gatepass/settings.py
+++ gatepass/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
+    
     'users.apps.UsersConfig',
+    'widget_tweaks',
     
 ]
 
